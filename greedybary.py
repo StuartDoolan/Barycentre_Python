c037a7df17514c3cd42bf46e2355d40ab6cd804f
StuartDoolan <stuartdoolan@Stuarts-MacBook-Air.local>
2016-11-02 13:48:23 +0000

    altered source input

--- greedybary.py
+++ greedybary.py
@@ -1,5 +1,5 @@
 #matplotlib inline
-
+from random import randint
 import numpy as np
 import sys
 import matplotlib.pyplot as plt
@@ -10,27 +10,35 @@ import greedy
 from get_barycentre import get_barycentre
 
   
-
+#creates time vector of 100 elements between values listed, far too few but just for tests
 tGPS = np.linspace(630720013, 1261872018)
 dt = tGPS[1]-tGPS[0]
 detector = 'gb'
-sourcealpha = np.linspace(0,2,10)
-sourcedelta = np.linspace(0, 1, 10)
+
+sourcealpha = np.zeros(100)
+sourcedelta = np.zeros(100)
+
+## creates rough 'random' source locations, adjust delta ones to be truly random
+for x in range(100):
+    sourcealpha[x] = randint(0,360)*np.pi/180
+    sourcedelta[x] = randint(-90, 90)*np.pi/180
+
 source = [sourcealpha, sourcedelta]
 sourcetest = [0.9, 0.622]
-wl = 7
-tssize = 1000 # training set size
+wl = 1024
+tssize = 100 # training set size
 TS = np.zeros((tssize, wl))
 
 for i in range(tssize):
     # get chirp masses
 
-    emit = get_barycentre(tGPS, detector, sourcetest, 'earth00-19-DE405.dat', 'sun00-19-DE405.dat') 
+    emit = get_barycentre(tGPS, detector, [source[0][i], source[1][i]], 'earth00-19-DE405.dat', 'sun00-19-DE405.dat') 
     [emitdt, emitte, emitdd, emitR, emitER, emitE, emitS] = emit
     TS[i]= emitdt[0][0]
     # normalise training set
     TS[i] /= np.sqrt(np.abs(greedy.dot_product(dt, TS[i], TS[i])))
     
+print('outloop')
 # tolerance for stopping algorithm
 tol = 1e-12
 
